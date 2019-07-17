a28dec55d2737e5d98549a37aae9fa07ef806598
Juanfran <juanfran.alcantara@kaleidos.net>
2019-07-17 13:15:40 +0200

    controls

--- flight-control.js
+++ flight-control.js
@@ -1,41 +1,44 @@
+function send(command) {
+  console.log(command);
+  send(command):
+}
+
 var FlightContol = {
   x: () => {
-    console.log('takeoff');
-    ws.send('takeoff');
+    send('takeoff');
   },
   y: () => {
-    ws.send('land');
+    send('land');
   },
   axismove: (event) => {
-    console.log(event);
-    console.log(event.target.getAttribute('id'));
-    const distance = 1;
+    const distance = 10;
     const horizontal = event.detail.axis[0];
     const vertical = event.detail.axis[1];
+    const noMoveMargin = 0.5;
 
     if (event.target.getAttribute('id') === 'left-control') {
-      if (horizontal > 0.2) {
-        ws.send(`cw 5`);
-      } else if (horizontal < -0.2) {
-        ws.send(`ccw 5`);
-      } else if (vertical < -0.2) {
-        ws.send(`up ${distance}`);
-      } else if (vertical > 0.2) {
-        ws.send(`down ${distance}`);
+      if (horizontal > noMoveMargin) {
+        send(`cw 5`);
+      } else if (horizontal < -noMoveMargin) {
+        send(`ccw 5`);
+      } else if (vertical < -noMoveMargin) {
+        send(`up ${distance}`);
+      } else if (vertical > noMoveMargin) {
+        send(`down ${distance}`);
       }
     } else {
-      if (horizontal > 0.2) {
-        ws.send(`right ${distance}`);
-      } else if (horizontal < -0.2) {
-        ws.send(`left ${distance}`);
-      } else if (vertical < -0.2) {
-        ws.send(`forward ${distance}`);
-      } else if (vertical > 0.2) {
-        ws.send(`back ${distance}`);
+      if (horizontal > noMoveMargin) {
+        send(`right ${distance}`);
+      } else if (horizontal < -noMoveMargin) {
+        send(`left ${distance}`);
+      } else if (vertical < -noMoveMargin) {
+        send(`forward ${distance}`);
+      } else if (vertical > noMoveMargin) {
+        send(`back ${distance}`);
       }
     }
 
-    console.log(event.detail.axis);
-//     ws.send('forward 1');
+   //  console.log(event.detail.axis);
+//     send('forward 1');
   }
 };
